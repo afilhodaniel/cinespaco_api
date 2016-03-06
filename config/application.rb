952800beee4d6ab17bb8c615fc2714a571899089
Daniel Filho <afilhodaniel@gmail.com>
2016-03-06 12:40:01 -0300

    Fix bug on dotenv gem

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 
 module CinespacoApi
   class Application < Rails::Application
-    Dotenv.load if Rails.env.development?
+    if Rails.env.development?
+        Dotenv.load 
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
